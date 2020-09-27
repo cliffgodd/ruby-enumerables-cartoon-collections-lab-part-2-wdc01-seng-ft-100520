738b9a756a7eb2674404eae70e1a5fb24c8ac1e1
Justin Clifford <justoon7.jc@gmail.com>
2020-09-27 23:35:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element|
+  planeteer_calls.map {|element|element.capitalize! += "!"}
     element.capitalize!
     element += "!"
 
